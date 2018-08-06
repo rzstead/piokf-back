98d4329901f2b6ba1be5a0ecd7809537a22d5f75
Ben Goff <benegoff@gmail.com>
2018-08-05 19:06:39 -0600

    Fixed and finish nav loading.

--- loadNav.php
+++ loadNav.php
@@ -1,42 +1,30 @@
 <?php
 
-("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 include "dbconfig.php";
 
 $pages = array();
 
 $parentPageQuery = "select * from pages where parent_page_id is null";
 $parentPagesResult = $mysqli->query($parentPageQuery);
-echo $parentPagesResult;
-// while($row = mysql_fetch_array($parentPagesResult))
-// {
-//     // $currentRoot = new stdClass();
-//     // $currentRoot->children = array();
-//     // $currentRoot->title = $row['title'];
-//     // $currentRootQuery = "select * from pages where parent_page_id = ".$row['id'];
-//     // $currentRootResult = $mysqli->query($currentRootQuery);
-//     // while($childRow = mysql_fetch_array($currentRootResult))
-//     // {
-//     //     $child = new stdClass();
-//     //     $child->title = $childRow['title'];
-//     //     array_push($currentRoot->children, $currentRoot);
-//     // }
-//     // array_push($pages, $currentRoot);
-// }
 
-// function addChildPages($result, $parentObject)
-// {
-//     while($row = mysql_fetch_array($result))
-//     {
-//        $currentRoot = new stdClass();
-//        $currentRoot->children = array();
-//        $currentRoot->title = $row['title'];
-//        $currentRootQuery = "select * from pages where parent_page_id = ".$row['id'];
-//        $currentRootResult = $mysqli->query($currentRootQuery);
-//        array_push($parentObject->children, $currentRoot);
-//        addChildPages($currentRootResult, $currentRoot);
-//     }
-// }
+while($row = mysqli_fetch_array($parentPagesResult))
+{
+    $currentRoot = new stdClass();
+    $currentRoot->id = $row['id'];
+    $currentRoot->children = array();
+    $currentRoot->title = $row['title'];
+    $currentRootQuery = "select * from pages where parent_page_id = ".$row['id'];
+    $currentRootResult = $mysqli->query($currentRootQuery);
+    while($childRow = mysqli_fetch_array($currentRootResult))
+    {
+        $child = new stdClass();
+        $child->title = $childRow['title'];
+        $child->id = $childRow['id'];
+        array_push($currentRoot->children, $child);
+    }
+    array_push($pages, $currentRoot);
+}
 
 echo json_encode($pages);
 
