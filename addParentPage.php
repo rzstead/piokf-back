0612e970979cfbbf9dfa84fb2988eac933d4d71d
Ben Goff <benegoff@gmail.com>
2018-08-09 11:56:12 -0600

    Separated add page into multiple endpoints.

---
+++ addParentPage.php
@@ -0,0 +1,29 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+include "dbconfig.php";
+
+session_start();
+
+if($_SESSION["isLoggedIn"]){
+    $data = json_decode(file_get_contents('php://input'));
+
+    $addPageQuery = "insert into pages (title) values ('"
+                        .$data->title."')";
+    
+    $pageAddResult = $mysqli->query($addPageQuery);
+    
+    /*
+        EXAMPLE JSON EXPECTED:
+        {
+            "title":"newPageTitle",
+            "parent_page_id":"3" //SET TO NULL IF PARENT
+        }
+    */
+} else {
+    echo "invalid login credentials";
+    header("HTTP/1.1 401 Unauthorized");
+    exit;
+}
+
+?>
