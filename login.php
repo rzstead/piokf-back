25a6a909b22f8a701e0d55a7b5634d811ecbfa85
Ben Goff <benegoff@gmail.com>
2018-08-12 13:57:19 -0600

    Allow headers?

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Headers: Authorization");
 include "dbconfig.php";
 
 //session_start();
